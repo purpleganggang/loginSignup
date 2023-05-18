f21daeb3a2820940f58ca2d5c18cc7826df6d188
myspringbootstyle <108319537+myspringbootstyle@users.noreply.github.com>
2023-05-18 21:25:55 +0900

    회원 가입폼 꾸미기

--- src/main/java/com/loginSignup/controller/MemberController.java
+++ src/main/java/com/loginSignup/controller/MemberController.java
@@ -1,46 +1,64 @@
 package com.loginSignup.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.loginSignup.constant.Role;
+import com.loginSignup.dto.MemberFormDto;
+import com.loginSignup.entity.Member;
+import com.loginSignup.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
+
 
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
-	
-	/**
-	 * 회원가입 폼페이지
-	 */
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
     @GetMapping(value = "/join")
     public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
 
 
-    /**
-     * 로그인 페이지
-     */
+    @PostMapping(value = "/join")
+    public String memberForm(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+
+        try{
+            Member member=Member.createMember(memberFormDto, passwordEncoder, Role.USER);
+            memberService.saveMember(member);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+        return "redirect:/";
+    }
+
+
     @GetMapping(value = "/login")
     public String loginMember(){
         return "member/memberLoginForm";
     }
 
-    
-    
-    /**
-     * 로그인 실패시 이동할 URL 을 설정
-     */
     @GetMapping(value = "/login/error")
     public String loginError(Model model){
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해 주세요.");
         return "member/memberLoginForm";
     }
-
 
 }
